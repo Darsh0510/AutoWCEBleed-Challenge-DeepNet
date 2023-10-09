faba0a8f967f50623d1e6d1ab4b65d8f1f09bf87
franklin654 <77013274+franklin654@users.noreply.github.com>
2023-10-09 10:50:52 +0530

    Update prediction_script.py

--- prediction_script.py
+++ prediction_script.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 
     #---------------------storing the endoscopy images--------------------------#
     for image_id in test_files:
-        img = cv2.imread(f'data/val/images/{image_id}') # change the path to images here
+        img = cv2.imread(os.path.join(args.dataset, image_id) # change the path to images here
         img = cv2.resize(img, (224,224))
         img_id = list(image_id.split('.'))[0]
         cv2.imwrite(f'debug/{img_id}.png',img) # change the directory here
